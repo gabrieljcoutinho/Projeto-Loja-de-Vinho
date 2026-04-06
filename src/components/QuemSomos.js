94e6d4320d70b7599a1393a5804ad8432aa41176
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-04-06 10:08:02 -0300

    quebrando linha

--- src/components/QuemSomos.js
+++ src/components/QuemSomos.js
@@ -16,7 +16,7 @@ const QuemSomos = () => {
               </p>
               Lorem Ipsum es simplemente el texto de relleno <br />
               de las imprentas y archivos de texto. Lorem<br />
-              Ipsum ha sido el texto de relleno estándar de las
+              Ipsum ha sido el texto de relleno estándar de las<br />
                industrias desde el año 1500, cuando un impresor
                 (N. del T. persona que se dedica a la imprenta)
                 desconocido usó una galería de textos y los
