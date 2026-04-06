cef4e3aca195022373ab3f23774ad4b0fdc3ed7b
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-04-06 16:43:35 -0300

    teste

--- src/components/Viniculas.js
+++ src/components/Viniculas.js
@@ -6,44 +6,57 @@ import wineTour from "../img/wineTour.png"
 import aperitivo from "../img/aperitivo.png"
 import coletaUva from "../img/coletaUva.png"
 
+const eventos = [
+  {
+    img: wineTour,
+    titulo: "WINE TOUR",
+    data: "12 Maio 2026",
+    info: "10:00 AM - Napa Valley"
+  },
+  {
+    img: aperitivo,
+    titulo: "APERITIVO",
+    data: "18 Maio 2026",
+    info: "18:00 PM - Toscana"
+  },
+  {
+    img: coletaUva,
+    titulo: "COLETA DE UVA",
+    data: "25 Maio 2026",
+    info: "08:00 AM - Mendoza"
+  }
+]
+
 const Viniculas = () => {
   return (
     <div className='viniculas'>
 
-            <h4 className='subTituloVinicula'>AUGUE SED NEC</h4>
-            <h3 className="tituloVinicula">VINEYARD EVENTS</h3>
+      <h4 className='subTituloVinicula'>AUGUE SED NEC</h4>
+      <h3 className="tituloVinicula">VINEYARD EVENTS</h3>
 
+      <div className="eventos">
 
-            <div className="eventos">
+        <div className="localidades">
+          {eventos.map((evento, index) => (
+            <div className="localidadeConteudo" key={index}>
+              <img src={evento.img} alt={evento.titulo} />
 
-            <div className="localidades">
+              <div className="conteudoEscrito">
+                <h4>{evento.titulo}</h4>
+                <p>{evento.data}</p>
+                <p>{evento.info}</p>
 
-                            <div className="localidadeConteudo">
-                                        <img src={wineTour} alt="" />
-
-
-                                                <div className="conteudoEscrito">
-                                                <h4>WINE TOUR</h4>
-                                                </div>
-
-                            </div>
-
-                            <div className="localidadeConteudo">
-                                    <img src={aperitivo} alt="" />
-                                </div>
-
-                                <div className="localidadeConteudo">
-                                        <img src={coletaUva} alt="" />
-                                </div>
-
-                </div>
-
-                <div className="imgVinicula">
-                            <img src={imgVinicula} alt="" />
-                </div>
-
+                <button>See More</button>
+              </div>
             </div>
+          ))}
+        </div>
 
+        <div className="imgVinicula">
+          <img src={imgVinicula} alt="Vinicula" />
+        </div>
+
+      </div>
 
     </div>
   )
