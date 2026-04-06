4e0914972024bca6d5ad63979ca2af63e78f2435
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-04-06 15:44:13 -0300

    conteudo abse

--- src/components/Viniculas.js
+++ src/components/Viniculas.js
@@ -8,6 +8,20 @@ const Viniculas = () => {
             <h4 className='subTituloVinicula'>AUGUE SED NEC</h4>
             <h3 className="tituloVinicula">VINEYARD EVENTS</h3>
 
+
+            <div className="eventos">
+
+            <div className="localidades">
+
+                </div>
+
+                <div className="imgVinicula">
+                    
+                </div>
+
+            </div>
+
+
     </div>
   )
 }
