54ac79113e576d3159a985b81a624df2b944964a
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-04-06 10:44:57 -0300

    conteudo

--- src/components/NossaHistoria.js
+++ src/components/NossaHistoria.js
@@ -10,7 +10,9 @@ const NossaHistoria = () => {
               <div className="conteudo">
               <h2 className='tituloNossaHistoria'>Our History</h2>
                 <p className="textoNossaHistoria">
-                    
+                Mattis aliquam malesuada euismod augue posuere venenatis. Maecenas faucibus dui vitae tellus et mi. Ut augue mattis augue dui et amet. Fringilla vestibulum dui, condimentum tristique duis.
+
+Phasellus ut sem montes, aliquam morbi accumsan nam aenean. Augue ut congue tincidunt eget commodo, tincidunt viverra.
                 </p>
               </div>
 
