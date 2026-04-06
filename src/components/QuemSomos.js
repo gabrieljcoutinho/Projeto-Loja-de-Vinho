1dacbd681e72f1463dc3ed6775b1b90997f067c0
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-04-06 10:08:20 -0300

    quebrando linha

--- src/components/QuemSomos.js
+++ src/components/QuemSomos.js
@@ -19,7 +19,7 @@ const QuemSomos = () => {
               Ipsum ha sido el texto de relleno estándar de las<br />
                industrias desde el año 1500, cuando un impresor<br />
                 (N. del T. persona que se dedica a la imprenta)<br />
-                desconocido usó una galería de textos y los
+                desconocido usó una galería de textos y los<br />
                  mezcló de tal manera que logró hacer un libro
                   de textos especimen. No sólo sobrevivió 500 años,
                   sino que tambien ingresó como texto de relleno
