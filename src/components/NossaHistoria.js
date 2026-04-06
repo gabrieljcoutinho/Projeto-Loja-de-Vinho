4b34972ec4354e7e15e1465b839e93cf96accee6
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-04-06 10:43:25 -0300

    img improtafa

--- src/components/NossaHistoria.js
+++ src/components/NossaHistoria.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import "../css/nossaHistoria.css"
 
+import imgNossaHistoria from "../img/imgNossaHistoria.png"
+
 const NossaHistoria = () => {
   return (
     <div className='nossaHistoria'>
 
                 <h2 className='tituloNossaHistoria'>Our History</h2>
+
+
+            <img src={imgNossaHistoria} alt="" />
 
     </div>
   )
