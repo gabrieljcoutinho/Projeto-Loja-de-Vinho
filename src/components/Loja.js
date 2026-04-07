6f5156b43973c9810364b38c434cc8945672137d
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-04-07 11:58:53 -0300

    valor do vinho

--- src/components/Loja.js
+++ src/components/Loja.js
@@ -24,7 +24,7 @@ const Loja = () => {
 
             <p className="NomeVinho">Barroco</p>
 
-            <p className="precoVinho">$ 83.920,00</p>
+            <p className="precoVinho">$ 83.920</p>
 
 
 
