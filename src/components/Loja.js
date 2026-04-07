d5094ce11f009d435f045e33e2a39a266e5499d1
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-04-07 17:28:29 -0300

    classes

--- src/components/Loja.js
+++ src/components/Loja.js
@@ -47,7 +47,7 @@ const Loja = () => {
             <ul className="descricaoVinho">
                 <li>Malbec 1987</li>
 
-                <li>Bodega | Mendoza</li>
+                <li className='itemListaDestaques'>Bodega | Mendoza</li>
 
                 <li className='itemListaDestaques'>Cosecha tardia</li>
     </ul>
