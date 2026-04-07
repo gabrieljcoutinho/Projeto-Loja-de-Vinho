d4f7d521522233f71352ff98425d7cb496ecd589
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-04-07 10:18:17 -0300

    novo vinho

--- src/components/Loja.js
+++ src/components/Loja.js
@@ -5,6 +5,7 @@ import imgVinho1 from "../img/vinho/vinhoBarroco.png"
 import imgVinho2 from "../img/vinho/vinhoParadigma.png"
 import imgVinho3 from "../img/vinho/vinhoQuieto.png"
 import imgVinho4 from "../img/vinho/vinhoParadigma2.png"
+import imgVinho5 from "../img/vinho/vinhoBarrocoRose.png"
 
 
 
