f5ce01a61e71ba1e93b2305b4cab5f45100e7795
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-04-10 09:46:35 -0300

    teste

--- src/components/Sucursale.js
+++ src/components/Sucursale.js
@@ -9,22 +9,22 @@ const Sucursale = () => {
   return (
     <div className='sucursale'>
 
-      <h2 className='tituloSucursale'>
-        <span className='spanSucursale'>Sucu</span>rsale
+      <h2 className='sucursale-title'>
+        <span>Sucu</span>rsale
       </h2>
 
-      <div className="viniculas">
+      <div className="sucursale-container">
 
-        <div className="viniculass">
-          <img src={imgVinicula1} alt="Vinícula 1" className='imgViniculas'/>
+        <div className="sucursale-card">
+          <img src={imgVinicula1} alt="Vinícola 1" />
         </div>
 
-        <div className="vinicula">
-          <img src={imgVinicula2} alt="Vinícula 2" className='imgViniculas'/>
+        <div className="sucursale-card">
+          <img src={imgVinicula2} alt="Vinícola 2" />
         </div>
 
-        <div className="vinicula">
-          <img src={imgVinicula3} alt="Vinícula 3" className='imgViniculas'/>
+        <div className="sucursale-card">
+          <img src={imgVinicula3} alt="Vinícola 3" />
         </div>
 
       </div>
