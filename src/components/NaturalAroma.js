47f8a57e32685d972b375b0cdcf295845048d484
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-04-10 07:48:53 -0300

    refatorando

--- src/components/NaturalAroma.js
+++ src/components/NaturalAroma.js
@@ -19,97 +19,80 @@ const NaturalAroma = () => {
   return (
     <div className='naturalAroma'>
 
-                <h3 className='subtitulo'>AUGUE SED NEC</h3>
+      <h3 className='subtitulo'>AUGUE SED NEC</h3>
+      <h2 className='titulo'>NATURAL AROMA</h2>
 
-                <h2 className='titulo'>NATURAL AROMA</h2>
-
-
-
-
-              <div className="flexboxGeral"></div>
-              <div className="flexboxNaturalAroma">
-
-<div className="itensNaturalAroma">
-<img src={img1} alt="" className='imgItemNaturalAroma' />
-</div>
-
-<div className="itensNaturalAroma">
-<img src={img2} alt="" className='imgItemNaturalAroma'/>
-</div>
-
-<div className="itensNaturalAroma">
-<img src={img3} alt="" className='imgItemNaturalAroma'/>
-</div>
-
-</div>
-
-
-<div className="flexboxNaturalAroma">
-
-<div className="itensNaturalAroma">
-<img src={img4} alt="" className='imgItemNaturalAroma'/>
-  </div>
-
-  <div className="itensNaturalAroma">
-  <img src={img5} alt="" className='imgItemNaturalAroma'/>
-</div>
-
-<div className="itensNaturalAroma">
-<img src={img6} alt="" className='imgItemNaturalAroma'/>
-</div>
-
-</div>
-
-
-        <div className="imgSeloMovimentacao">
-          <img src={imgSelo} alt="" />
+      <div className="flexboxNaturalAroma">
+        <div className="itensNaturalAroma">
+          <img src={img1} alt="Vinho 1" className='imgItemNaturalAroma' />
         </div>
 
-        <div className="naturaSelo">
-
-        <div className="naturaSelo">
-
-<div className="coluna">
-  <div className="item">
-    <img src={imgSeloNatura1} className="selos" />
-    <div>
-      <h5>Wine Glass</h5>
-      <p>Lorem ipsum malesuada</p>
-    </div>
-  </div>
-
-  <div className="item">
-    <img src={imgSeloNatura2} className="selos" />
-    <div>
-      <h5>Fine Smell</h5>
-      <p>Lorem ipsum malesuada</p>
-    </div>
-  </div>
-</div>
-
-<div className="coluna">
-  <div className="item">
-    <img src={imgSeloNatura3} className="selos" />
-    <div>
-      <h5>Unique Sort</h5>
-      <p>Lorem ipsum malesuada</p>
-    </div>
-  </div>
-
-  <div className="item">
-    <img src={imgSeloNatura4} className="selos" />
-    <div>
-      <h5>Old Habit</h5>
-      <p>Lorem ipsum malesuada</p>
-    </div>
-  </div>
-</div>
-
-</div>
-
-
+        <div className="itensNaturalAroma">
+          <img src={img2} alt="Vinho 2" className='imgItemNaturalAroma'/>
         </div>
 
+        <div className="itensNaturalAroma">
+          <img src={img3} alt="Vinho 3" className='imgItemNaturalAroma'/>
+        </div>
+      </div>
+
+      <div className="flexboxNaturalAroma">
+        <div className="itensNaturalAroma">
+          <img src={img4} alt="Vinho 4" className='imgItemNaturalAroma'/>
+        </div>
+
+        <div className="itensNaturalAroma">
+          <img src={img5} alt="Vinho 5" className='imgItemNaturalAroma'/>
+        </div>
+
+        <div className="itensNaturalAroma">
+          <img src={img6} alt="Vinho 6" className='imgItemNaturalAroma'/>
+        </div>
+      </div>
+
+      <div className="imgSeloMovimentacao">
+        <img src={imgSelo} alt="Selo decorativo" />
+      </div>
+
+      <div className="naturaSelo">
+
+        <div className="coluna">
+          <div className="item">
+            <img src={imgSeloNatura1} className="selos" alt="Wine Glass" />
+            <div>
+              <h5>Wine Glass</h5>
+              <p>Lorem ipsum malesuada</p>
+            </div>
+          </div>
+
+          <div className="item">
+            <img src={imgSeloNatura2} className="selos" alt="Fine Smell" />
+            <div>
+              <h5>Fine Smell</h5>
+              <p>Lorem ipsum malesuada</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="coluna">
+          <div className="item">
+            <img src={imgSeloNatura3} className="selos" alt="Unique Sort" />
+            <div>
+              <h5>Unique Sort</h5>
+              <p>Lorem ipsum malesuada</p>
+            </div>
+          </div>
+
+          <div className="item">
+            <img src={imgSeloNatura4} className="selos" alt="Old Habit" />
+            <div>
+              <h5>Old Habit</h5>
+              <p>Lorem ipsum malesuada</p>
+            </div>
+          </div>
+        </div>
+
+      </div>
 
     </div>
   )
